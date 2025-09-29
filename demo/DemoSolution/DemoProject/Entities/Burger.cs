1d17217a7ce06fcd6450e095404e6ef2b73dc458
JP <jp@jp.nl>
2025-09-29 11:24:57 +0200

    Projecten besproken/gemaakt, databinding laten zien

---
+++ demo/DemoSolution/DemoProject/Entities/Burger.cs
@@ -0,0 +1,9 @@
+﻿namespace DemoProject.Entities;
+
+public class Burger
+{
+    public string Name { get; set; }
+    public decimal Price { get; set; }
+    public decimal Rating { get; set; }
+    public string PhotoUrl { get; set; }
+}
